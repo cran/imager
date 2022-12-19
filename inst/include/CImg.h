d76b3e0815a374aabb36fac1bd2e816f1e6cf049
Shota Ochi <shotaochi1990@gmail.com>
2022-12-19 07:30:02 +0000

    version 0.42.16

--- inst/include/CImg.h
+++ inst/include/CImg.h
@@ -2354,16 +2354,6 @@ namespace cimg_library_suffixed {
       return result;
     }
 
-    inline int _sprintf(char *const s, const char *const format, ...) {
-      cimg::mutex(6);
-      va_list args;
-      va_start(args,format);
-      const int result = std::vsprintf(s,format,args);
-      va_end(args);
-      cimg::mutex(6,0);
-      return result;
-    }
-
     inline int _snprintf(char *const s, const size_t n, const char *const format, ...) {
       cimg::mutex(6);
       va_list args;

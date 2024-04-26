ec0bda27eb95de63d338b6f219bea7ab5caece66
Aaron Robotham <aaron.robotham@uwa.edu.au>
2024-04-26 10:40:03 +0000

    version 1.0.1

--- R/datasets.R
+++ R/datasets.R
@@ -2,5 +2,5 @@
 ##'
 ##' This photograph was downloaded from http://r0k.us/graphics/kodak/kodim09.html. Its size was reduced by half to speed up loading and save space.
 ##' @format an image of class cimg
-##' @source \url{ http://r0k.us/graphics/kodak/kodim09.html }
+##' @source http://r0k.us/graphics/kodak/kodim09.html
 "boats"
